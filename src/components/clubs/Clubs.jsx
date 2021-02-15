18944cf95dd46e593e8aa93957ede318f7c2e062
malli68 <42874701+malli68@users.noreply.github.com>
2021-02-15 18:31:48 +0530

    Update Clubs.jsx

--- src/components/clubs/Clubs.jsx
+++ src/components/clubs/Clubs.jsx
@@ -2,17 +2,26 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import Footer from "../commons/Footer";
 import Header from "../commons/Header";
-import { setdata, adduser, getuser,deleteuser } from "../redux/actions/ClubsActions";
+import {
+  setdata,
+  adduser,
+  getuser,
+  deleteuser,getclubinfo,updateclub,
+} from "../redux/actions/ClubsActions";
 export class Clubs extends Component {
   constructor(props) {
     super(props);
-    this.state={
-    club_list:[]
-}
-  
+    this.state = {
+      club_list: [],
+      getclubinfo:[]
+      
+      
+    };
   }
 
   render() {
+   console.log(getclubinfo) 
+   var getclubinfo=this.props.getclubinfo()
     var {
       name,
       username,
@@ -22,8 +31,11 @@ export class Clubs extends Component {
       is_create,
       club_type,
       club_list,
+      clublocation,
+      clubname,
+      superAdminId,club_details,
     } = this.props;
-
+    console.log("club_details", club_details)
     return (
       <div>
         <div class="container-fluid">
@@ -73,25 +85,6 @@ export class Clubs extends Component {
                         <span>Clubs</span>
                       </a>
                     </li>
-
-                    {/*   <li>
-                    <a href="/clubusers" class=" waves-effect">
-                        <i class="mdi mdi-calendar-text"></i>
-                        <span>Club Users</span>
-                    </a>
-                </li> */}
-                    {/* 
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="mdi mdi-file-tree"></i>
-                        <span>Cash Games</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="true">
-                        <li><a href="javascript: void(0);">Point Rummy</a></li>
-                        <li><a href="javascript: void(0);">Pool Rummy</a></li>
-                        <li><a href="javascript: void(0);">Deal Rummy</a></li>
-                    </ul>
-                </li> */}
                   </ul>
                 </div>
                 {/* <!-- Sidebar --> */}
@@ -135,6 +128,7 @@ export class Clubs extends Component {
                                 <input
                                   type="text"
                                   value={name}
+                                  ref="name"
                                   id="name"
                                   name="name"
                                   class="form-control"
@@ -149,6 +143,7 @@ export class Clubs extends Component {
                                   type="email"
                                   value={email}
                                   id="email"
+                                  ref="email"
                                   name="email"
                                   class="form-control"
                                   onChange={this.handleChange}
@@ -161,7 +156,8 @@ export class Clubs extends Component {
                                 <input
                                   type="text"
                                   value={mobile}
-                                  id="subject"
+                                  id="mobile"
+                                  ref="mobile"
                                   name="mobile"
                                   class="form-control"
                                   onChange={this.handleChange}
@@ -173,7 +169,8 @@ export class Clubs extends Component {
                                 <input
                                   type="text"
                                   value={username}
-                                  id="subject"
+                                  id="username"
+                                  ref="username"
                                   class="form-control"
                                   name="username"
                                   onChange={this.handleChange}
@@ -185,7 +182,8 @@ export class Clubs extends Component {
                                 <select
                                   className="form-control"
                                   name="club_type"
-                                  /* value={club_type} */ onChange={
+                                  ref="club_type"
+                                   value={club_type}  onChange={
                                     this.handleChange
                                   }
                                   required
@@ -204,6 +202,7 @@ export class Clubs extends Component {
                                   type="button"
                                   class="btn btn-primary btn-block"
                                   value="Create"
+                                  ref="Create"
                                   onClick={() => {
                                     this.createClub();
                                   }}
@@ -248,27 +247,13 @@ export class Clubs extends Component {
                           </thead>
 
                           <tbody>
-                            {/*      <tr>
-                                                <td>1</td>
-                                                <td>{name}</td>
-                                                <td>08/02/2020</td>
-                                                <td>{mobile}</td>
-                                                <td>{email}</td>
-                                                <td>{username}</td>
-                                                <td>240</td>
-                                                <td>200</td>
-                                                <td>
-                                                    <i class="fa fa-edit"></i>
-                                                    <i class="fa fa-trash"></i>
-                                                </td>
-                                            </tr> */}
-                                          
                             {club_list.length > 0 ? (
-                             club_list.map((club, index) => {
-                                console.log(club._id)
+                              club_list.map((club, index) => {
+                                // console.log(club._id);
+                                // console.log(club.mobileno)
                                 return (
                                   <tr key={index}>
-                                    <td>{index +1}</td>
+                                    <td>{index + 1}</td>
                                     {/* <td>{ club._id }</td> */}
                                     <td>{club.clubname}</td>
                                     <td>08/02/2020</td>
@@ -278,8 +263,14 @@ export class Clubs extends Component {
                                     <td>240</td>
                                     <td>200</td>
                                     <td>
-                                    <i class="fa fa-edit" onClick={()=>this.onEdit(club)}></i>
-                                    <i class="fa fa-trash" onClick={()=>this.onDelete(club._id)}></i>
+                                     <i
+                                        class="fa fa-edit" data-toggle="modal" data-target="#myModal"
+                                        onClick={() => this.onEdit(club._id)}
+                                      ></i>
+                                      <i
+                                        class="fa fa-trash"
+                                        onClick={() => this.onDelete(club._id)}
+                                      ></i>
                                     </td>
                                   </tr>
                                 );
@@ -298,6 +289,127 @@ export class Clubs extends Component {
                 </div>
                 {/* <!-- end row --> */}
               </div>
+              <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          {/* <h4 class="modal-title">Update Club</h4> */}
+        </div>
+        <div class="modal-body">
+
+
+        <div>
+                 
+                 <div class="row">
+                   <div class="col-12">
+                     <div class="card box-big-shadow">
+                       <h4 class="card-header mt-0">Update Player</h4>
+                       <div class="card-body">
+                         <form class="repeater" enctype="multipart/form-data">
+                           <div data-repeater-list="group-a">
+                             <div data-repeater-item class="row">
+                               <div class="form-group col-lg-8">
+                                 <label for="name">Name</label>
+                                 <input
+                                   type="text"
+                                   value={club_details.clubname}
+                                   ref="name"
+                                   id="name"
+                                   name="name"
+                                   class="form-control"
+                                   onChange={this.handleChange}
+                                   required
+                                 />
+                               </div>
+ 
+                               <div class="form-group col-lg-4">
+                                 <label for="email">Email</label>
+                                 <input
+                                   type="email"
+                                   value={club_details.email}
+                                   id="email"
+                                   ref="email"
+                                   name="email"
+                                   class="form-control"
+                                   onChange={this.handleChange}
+                                   required
+                                 />
+                               </div>
+ 
+                               <div class="form-group col-lg-4">
+                                 <label for="subject">Mobile</label>
+                                 <input
+                                   type="text"
+                                   value={club_details.mobileno}
+                                   id="mobile"
+                                   ref="mobile"
+                                   name="mobile"
+                                   class="form-control"
+                                   onChange={this.handleChange}
+                                   required
+                                 />
+                               </div>
+                               <div class="form-group col-lg-4">
+                                 <label for="subject">Username</label>
+                                 <input
+                                   type="text"
+                                   value={club_details.username}
+                                   id="username"
+                                   ref="username"
+                                   class="form-control"
+                                   name="username"
+                                   onChange={this.handleChange}
+                                   required
+                                 />
+                               </div>
+                               <div class="form-group col-lg-4">
+                                 <label for="subject">Club-Types</label>
+                                 <select
+                                   className="form-control"
+                                   name="club_type"
+                                   ref="club_type"
+                                   value={club_details.clubtype}
+                                   onChange={this.handleChange}
+                                   required
+                                 >
+                                   <option>Clubtypes</option>
+                                   <option value="0">Bronze</option>
+                                   <option value="1">Silver</option>
+                                   <option value="2">Gold</option>
+                                   <option value="3">Diamond</option>
+                                   <option value="4">Platinum</option>
+                                 </select>
+                               </div>
+                               <div class="col-lg-3 align-self-center">
+                                 <input
+                                   data-repeater-create
+                                   type="button"
+                                   class="btn btn-primary btn-block"
+                                   value="Update"
+                                   ref="Create"
+                                   onClick={() => {
+                                    this.updateClub(club_details);
+                                   }}
+                                 ></input>
+                               </div>
+                             </div>
+                           </div>
+                         </form>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+                 </div>
+
+        </div>
+        {/* <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal" style={{backgroundColor:"Highlight"}} onClick={()=>this.updateClub(club_list._id)}>Submit</button>
+        </div> */}
+      </div>
+      
+    </div>
+  </div>
               {/* <!-- End Page-content --> */}
               <Footer />
             </div>
@@ -308,6 +420,12 @@ export class Clubs extends Component {
       </div>
     );
     // <div class="rightbar-overlay"></div>
+  }
+  updateClub=(_id)=>{
+    console.log(_id)
+    this.props.updateclub(_id);
+    this.props.getuser(_id)
+
   }
   handleChange = (e) => {
     var data = { name: e.target.name, value: e.target.value };
@@ -325,33 +443,56 @@ export class Clubs extends Component {
   };
   componentDidMount() {
      this.props.getuser();
-  
+    
   }
+
+// componentWillReceiveProps(nextProps){
+//   console.log(nextProips)
+// }
 
   componentDidUpdate(prevProps, prevState) {
     // check whether client has
-    this.props.deleteuser();
     const { submit_success, is_create, club_list } = this.props;
   }
-  onDelete(_id){
-this.props.deleteuser()
+  onDelete(_id) {
+    console.log(_id)
+    this.props.deleteuser({id:_id});
+    this.props.getuser(_id)
+  }
+  onEdit(_id) {
+    console.log(_id)
+    console.log(getclubinfo._id)
+    // this.props.deleteuser({id:_id});
+    this.props.getclubinfo({id:_id})
   }
   createClub() {
-    const { name, username, mobile, email, club_type } = this.props;
-    // const {username,password}=state.auth
+    const {
+      name,
+      username,
+      mobile,
+      email,
+      club_type,
+      clublocation,
+      password,
+      superAdminId,
+    } = this.props;
     console.log(data);
     var data = {
       clubname: name,
       clubtype: club_type,
-      clublocation: "",
+      clublocation: "hyd",
       mobileno: mobile,
       email: email,
-      username: /* this.state.auth("username") */ "",
-      password: /* this.state.auth("password") */ "",
-      superAdminId: "",
+      username: username,
+      password: "123456",
+      // superAdminId: "123",
     };
     this.props.adduser(data);
-   // this.props.getuser(data);
+    this.props.getuser(data);
+    console.log(this.props.getclubinfo(data),"getClubInfo");
+    console.log(getclubinfo)
+
+
   }
 }
 const mapStateToProps = (state) => {
@@ -362,13 +503,18 @@ const mapStateToProps = (state) => {
     mobile,
     club_type,
     error,
+    clublocation,
+    clubname,
+    superAdminId,
     popup_msg,
     loading,
     submit_success,
     is_create,
     item_deleted,
-    club_list,
+    club_list,club_details
   } = state.clubs;
+
+  console.log("club_details", loading)
 
   return {
     name,
@@ -377,14 +523,18 @@ const mapStateToProps = (state) => {
     mobile,
     club_type,
     error,
+    clublocation,
+    clubname,
+    superAdminId,
     popup_msg,
     loading,
     submit_success,
     is_create,
     item_deleted,
-    club_list,
+    club_list,club_details
   };
 };
+
 const mapDispatchToProps = (dispatch) => {
   return {
     setdata: (input) => {
@@ -396,9 +546,15 @@ const mapDispatchToProps = (dispatch) => {
     getuser: (input) => {
       dispatch(getuser(input));
     },
-    deleteuser:(input) =>{
-        dispatch(deleteuser(input));
-    }
+    deleteuser: (input) => {
+      dispatch(deleteuser(input));
+    },
+     getclubinfo: (input) =>{
+      dispatch(getclubinfo(input));
+    } ,
+    updateclub: (input) =>{
+      dispatch(updateclub(input));
+    } 
   };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Clubs);
