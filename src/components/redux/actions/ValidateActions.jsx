440f3adb506adf6b1fda0d23601cb1b22e98964d
malli68 <42874701+malli68@users.noreply.github.com>
2021-02-16 18:21:20 +0530

    Update ValidateActions.jsx

--- src/components/redux/actions/ValidateActions.jsx
+++ src/components/redux/actions/ValidateActions.jsx
@@ -7,46 +7,24 @@ const SESSION_KEY_NAME = config.SESSION_KEY_NAME;
 
 export const checkUserNameExist = (i) => {
   return (dispatch) => {
-    const user = getCacheObject(SESSION_KEY_NAME);
-    let data = { username: i.value };
-    axios
-      .post(ServiceUrls.CHECK_CLIENT_USERNAME_EXIST, data, {
-        headers: { "access-token": user["access-token"] },
-      })
+    const userinfo= getCacheObject(SESSION_KEY_NAME);
+    console.log("hiiiii");
+    console.log(i);
+    let data = { username: i };
+    console.log(data);
+    console.log(userinfo);
+    
+    
+    axios.post(ServiceUrls.CHECK_USERNAME, data,{ headers: { "Authorization": "Bearer "+userinfo['token'] }})
       .then((res) => {
         console.log("get>>", res.data);
-        let resCode = res.data.status;
+        let resCode = res.data.code;
         if (resCode === 200) {
-          dispatch(checkuserExist(i, res.data.response.message, resCode, 1)); //1==> username
+          dispatch(checkuserExist(i, res.data.message, resCode, 1)); //1==> username
         } else if (resCode === 400) {
-          dispatch(checkuserExist(i, res.data.response.message, resCode, 1));
+          dispatch(checkuserExist(i, res.data.message, resCode, 1));
         } else {
-          dispatch(request_fail(res.data.response.message));
-        }
-      })
-      .catch((err) => {
-        dispatch(request_fail(err.message));
-      });
-  };
-};
-
-export const checkPhoneNumberExist = (i) => {
-  return (dispatch) => {
-    const user = getCacheObject(SESSION_KEY_NAME);
-    let data = { mobile: i.value };
-    axios
-      .post(ServiceUrls.CHECK_CLIENT_PHONE_NO_EXIST, data, {
-        headers: { "access-token": user["access-token"] },
-      })
-      .then((res) => {
-        console.log("get>>", res.data);
-        let resCode = res.data.status;
-        if (resCode === 200) {
-          dispatch(checkuserExist(i, res.data.response.message, resCode, 2)); //1==> username
-        } else if (resCode === 400) {
-          dispatch(checkuserExist(i, res.data.response.message, resCode, 2));
-        } else {
-          dispatch(request_fail(res.data.response.message));
+          dispatch(request_fail(res.data.data.message));
         }
       })
       .catch((err) => {
@@ -57,73 +35,6 @@ export const checkPhoneNumberExist = (i) => {
 
 
 
-export const checkEmailExist = (i) => {
-  return (dispatch) => {
-    const user = getCacheObject(SESSION_KEY_NAME);
-    let data = { email: i.value };
-    axios
-      .post(ServiceUrls.CHECK_CLIENT_EMAIL_EXIST, data, {
-        headers: { "access-token": user["access-token"] },
-      })
-      .then((res) => {
-        console.log("get>>", res.data);
-        let resCode = res.data.status;
-        if (resCode === 200) {
-          dispatch(checkuserExist(i, res.data.response.message, resCode, 3)); //1==> username
-        } else if (resCode === 400) {
-          dispatch(checkuserExist(i, res.data.response.message, resCode, 3));
-        } else {
-          dispatch(request_fail(res.data.response.message));
-        }
-      })
-      .catch((err) => {
-        dispatch(request_fail(err.message));
-      });
-  };
-};
-
-export const checkBraceletExist = (i) => {
-  return (dispatch) => {
-    const user = getCacheObject(SESSION_KEY_NAME);
-    let data = { device_slno: i.value };
-    axios
-      .post(ServiceUrls.CHECK_BRACELET_DUPLICATE, data, {
-        headers: { "access-token": user["access-token"] },
-      })
-      .then((res) => {
-        console.log("get>>", res.data);
-        let resCode = res.data.status;
-        if (resCode === 200) {
-          dispatch(setdataTobraceletmsg(res.data.response.message)); //1==> username
-        } else if (resCode === 400) {
-          dispatch(setdataTobraceletmsg(res.data.response.message));
-        } else {
-          dispatch(request_fail(res.data.response.message));
-        }
-      })
-      .catch((err) => {
-        dispatch(request_fail(err.message));
-      });
-  };
-};
-
-export const setvalidationemaildata = (i) => {
-  return (dispatch) => {
-    dispatch(setdataToemailmsg(i));
-  };
-};
-
-export const setvalidationbraceletdata = (i) => {
-  return (dispatch) => {
-    dispatch(setdataTobraceletmsg(i));
-  };
-};
-
-export const setvalidationphonedata = (i) => {
-  return (dispatch) => {
-    dispatch(setdataTophonemsg(i));
-  };
-};
 
 export const resetvalidatedata = () => {
   return (dispatch) => {
@@ -133,35 +44,20 @@ export const resetvalidatedata = () => {
 
 const checkuserExist = (i, data, code, errtype) => ({
   type: ValidationActionTypes.VALIDATION_REQUEST_SUCCESS,
-  i: i,
   payload: data,
-  code: code,
-  errtype: errtype,
 });
 
-export const checkVictimExist = (data) => ({
-  type: ValidationActionTypes.VALIDATION_VICTIM_REQUEST_SUCCESS,
-  payload: data,
-});
+
 
 const request_fail = (data) => ({
   type: ValidationActionTypes.VALIDATION_REQUEST_FAIL,
   payload: data,
 });
 
-const setdataToemailmsg = (data) => ({
-  type: ValidationActionTypes.SET_VALIDATION_EMAIL_DATA,
-  payload: data,
-});
-export const setdataTobraceletmsg = (data) => ({
-  type: ValidationActionTypes.SET_VALIDATION_BRACELET_DATA,
-  payload: data,
-});
 
-const setdataTophonemsg = (data) => ({
-  type: ValidationActionTypes.SET_VALIDATION_PHONE_DATA,
-  payload: data,
-});
+
+
+
 
 const reset_data = () => ({
   type: ValidationActionTypes.RESET_VALIDATION_DATA,
